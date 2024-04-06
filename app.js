6f17042efb68866b0c497fffda6611143015e46f
Mario Faria <msgfaria@gmail.com>
2024-04-06 02:16:44 -0300

    Edição

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //let titulo = document.querySelector('h1')
-//titulo.innerHTML = 'Jogo do número secreto';
+//titulo.innerHTML = 'Jogo do número secreto!';
 
 let listaDeNumerosSorteados = [];
 let numeroLimite = 10;
